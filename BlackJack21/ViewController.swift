e37c227b4be152e7bc71e4765b71eb7a058c91f5
Yoga <jc051533@gmail.com>
2023-08-01 18:59:25 +0800

    Initial Commit

---
+++ BlackJack21/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BlackJack21
+//
+//  Created by 王鈞則 on 2023/8/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
